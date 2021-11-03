d7f51ad63cc98543ea0302506e2db6461af3a425
karlajaramillo <karlav.jaramillo@gmail.com>
2021-11-03 22:02:42 +0100

    Add style, images, routes

---
+++ app.js
@@ -0,0 +1,31 @@
+// import express to include in our project
+const express = require('express');
+// initialize the server express, app is the instance of express
+const app = express();
+
+// make everything inside the 'public' available
+// middleware --> express.static  --> set static files from public directory
+app.use(express.static('public'));
+
+// set the routes to show to the user
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/public/views/home.html`);
+});
+
+
+app.get("/about", (req, res) => {
+  //res.send('hello monina')
+  res.sendFile(`${__dirname}/public/views/about.html`);
+});
+
+app.get("/works", (req, res) => {
+  res.sendFile(`${__dirname}/public/views/works.html`);
+});
+
+app.get("/gallery", (req, res) => {
+  //res.send('hello monina')
+  res.sendFile(`${__dirname}/public/views/gallery.html`);
+});
+
+// listen to a port
+app.listen(3000, ()=> console.log('listeting to the port 3000'));
