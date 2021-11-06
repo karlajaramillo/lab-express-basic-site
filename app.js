1e2742fb0c4b3cc07b99892bf9943d95de806da5
karlajaramillo <karlav.jaramillo@gmail.com>
2021-11-06 10:33:00 +0100

    Add path and page not found

--- app.js
+++ app.js
@@ -1,38 +1,45 @@
 // import express to include in our project
 const express = require('express');
+// normalize the path in every systems
+const path = require('path');
 // initialize the server express, app is the instance of express
 const app = express();
 
-const mongo = require("hbs")
-
-// creates an absolute path pointing to a folder called 'views'
-// tell our Express that HBS will be in charge of the rendering HTML
-app.set("views", `${__dirname}/views`);
-app.set("view engine", "hbs");
 
 // make everything inside the 'public' available
-// middleware --> express.static  --> set static files from public directory
+// middleware --> express.static  
+//--> set static files in the public folder
 app.use(express.static('public'));
 
 // set the routes to show to the user
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/public/views/home.html`);
+  //res.sendFile(`${__dirname}/public/views/home.html`);
+  // without 'path'
+  //res.sendFile(`${__dirname}/public/views/home.html`);
+  res.sendFile(path.join(__dirname, 'public', 'views', 'home.html'));
 });
 
 
 app.get("/about", (req, res) => {
-  //res.send('hello monina')
-  res.sendFile(`${__dirname}/public/views/about.html`);
+  //res.sendFile(`${__dirname}/public/views/about.html`);
+  res.sendFile(path.join(__dirname, 'public', 'views', 'about.html'));
 });
 
 app.get("/works", (req, res) => {
-  res.sendFile(`${__dirname}/public/views/works.html`);
+  //res.sendFile(`${__dirname}/public/views/works.html`);
+  res.sendFile(path.join(__dirname, 'public', 'views', 'works.html'));
+
 });
 
 app.get("/gallery", (req, res) => {
-  //res.send('hello monina')
-  res.sendFile(`${__dirname}/public/views/gallery.html`);
+  //res.sendFile(`${__dirname}/public/views/gallery.html`);
+  res.sendFile(path.join(__dirname, 'public', 'views', 'gallery.html'));
 });
+
+// send an error to user if there is no url defined
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "views", "not-found.html"));
+})
 
 // listen to a port
 app.listen(3000, ()=> console.log('listeting to the port 3000'));
